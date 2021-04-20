86ba1ecd193f39ffca501b086274fcfdafb0ce30
Denys Savchenko <speleoden@gmail.com>
2021-04-20 20:58:43 +0200

    external data

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,21 @@ ADD antares-backend/antares_data_server_wd /antares_data_server
 RUN pip3 install -r /antares_data_server/requirements.txt && \
     pip3 install /antares_data_server
 
+RUN pip3 install git+https://git.km3net.de/open-data/openkm3
+
 ADD antares-backend/antares_environment /workdir/antares_environment
 
-RUN mkdir -p /workdir/antares_environment/antares_bin; mkdir -p /workdir/antares_environment/antares_output; cd /workdir/antares_environment/antares_src; bash make.sh
+RUN mkdir -p /workdir/antares_environment/antares_bin; \
+    mkdir -p /workdir/antares_environment/antares_output; \
+    cd /workdir/antares_environment/antares_src; bash make.sh; \
+    chmod o+w /workdir/antares_environment/antares_output; \
+    rm /workdir/antares_environment/antares_data/*; \
+    chmod o+w /workdir/antares_environment/antares_data
 
 WORKDIR /workdir/antares_environment
 
 ENV ANTARES_CONFIG_FILE=/antares_data_server/antares_data_server/config_dir/config.yml
 
+ADD populate_data.py /antares/populate_data.py
 ADD entrypoint.sh /antares/entrypoint.sh
 ENTRYPOINT [ "/bin/bash", "/antares/entrypoint.sh" ]
---
+++ populate_data.py
@@ -0,0 +1,42 @@
+import argparse
+from openkm3.store import KM3Store
+from antares_data_server.backend_api import Configurer
+import os
+import pandas as pd
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-conf_file', type=str, default=None)
+    args = parser.parse_args()
+    conf_file = args.conf_file
+
+    conf = Configurer.from_conf_file(conf_file)
+
+    store = KM3Store()
+    interpolation = store.get("ana20_01_bkg")
+    with open(os.path.join(conf.root_wd, 
+                           conf.antares_env_dir, 
+                           conf.data_dir, 
+                           'background.txt')) as fd:
+        print(*interpolation.data, sep=' ', file=fd)
+    
+    lookup = store.get('ana20_01_acc')
+    acc_df = pd.DataFrame.from_dict(lookup.data)
+    with open(os.path.join(conf.root_wd, 
+                           conf.antares_env_dir, 
+                           conf.data_dir, 
+                           'acc.txt')) as fd:
+        acc_df.to_csv(fd, sep=' ', index=False, header=False)
+    
+    data = store.get('ana20_01_vo')
+    data_df = data.get_dataframe()
+
+    with open(os.path.join(conf.root_wd, 
+                           conf.antares_env_dir, 
+                           conf.data_dir, 
+                           'ANTARES.data')) as fd:
+        data_df.to_csv(fd, columns=['ra', 'decl', 'nhit', 'beta', 'mjd'], 
+                       sep='\t', index=False, header=False)
+     
+if __name__ == '__main__':
+    main()
