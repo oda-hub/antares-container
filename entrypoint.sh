d08e53995e8dde2ac2112d2baf3a23765b3b4dc8
Denys Savchenko <speleoden@gmail.com>
2021-04-20 14:12:30 +0200

    update backend

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ ls -tlroa /var/log/containers/
 python3 /antares_data_server/bin/run_antares_back_end.py \
         -conf_file $ANTARES_CONFIG_FILE \
         -debug \
-        -use_gunicorn 2>&1 | tee -a /var/log/containers/${CONTAINER_NAME} -
+        -use_gunicorn 2>&1 | tee -a /var/log/containers/${CONTAINER_NAME:-`hostname`}
